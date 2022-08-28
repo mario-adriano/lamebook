d8973ffb697476218a710b416d1b3c7bfd025e3c
Mário <mario-adriano@github.com>
2022-08-28 19:29:38 -0300

    add tailwind and bootstrap icon

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,15 @@
 class LikesController < ApplicationController
   def create
     post = Post.find_by_id(like_params['post_id'])
-    @like = Like.new(user: current_user, post: post)
+    @like = Like.new(
+              user: current_user, 
+              post: post
+            )
 
     unless @like.save
       flash[:notice] = @like.errors.full_messages.to_sentence
     end
-    
+
     respond_to do |format|
       if turbo_frame_request? && turbo_frame_request_id == "like-dislike-#{post.id}"
         format.html { render partial: 'like', locals: { post: post, like_count: Like.size_likes_post(post) } }
@@ -18,7 +21,12 @@ class LikesController < ApplicationController
 
   def destroy
     post = Post.find_by_id(id_params)
-    if @like = Like.find_by(user: current_user, post: post)
+    
+    if @like = Like.find_by(
+                user: current_user, 
+                post: post
+              )
+      
       @like.delete
     
       respond_to do |format|
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   has_many :likes
 
   self.per_page = 20
+
+  def username = user.username;
 end
