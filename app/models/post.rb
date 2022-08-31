59ac58b2f3c6b46b329f16e19fe75be1789e7c8f
mario-adriano <mariosergioadriano@gmail.com>
2022-08-31 09:55:52 -0300

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: posts
+#
+#  id                :integer          not null, primary key
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  content           :text
+#  likes             :like
+#  user_id           :integer          not null
+#
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
